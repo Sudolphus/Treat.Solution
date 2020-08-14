d0b8a688330cc107d436d8cc04a54d2d96886443
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-14 09:21:34 -0700

    add home controller

--- Treat/Controllers/HomeController.cs
+++ Treat/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Treat.Models;
 
 namespace Treat.Controllers
@@ -15,9 +16,28 @@ namespace Treat.Controllers
     }
 
     [HttpGet("/")]
-    public ActionResult Index(string treatName, string flavorName)
+    public ActionResult Index(string foodName, string flavorName)
     {
       IQueryable<Food> foodQuery = _db.Foods;
+      IQueryable<Flavor> flavorQuery = _db.Flavors;
+      if (!string.IsNullOrEmpty(foodName))
+      {
+        Regex foodSearch = new Regex(foodName, RegexOptions.IgnoreCase);
+        foodQuery = foodQuery.Where(f => foodSearch.IsMatch(f.Name));
+      }
+      if (!string.IsNullOrEmpty(flavorName))
+      {
+        Regex flavorSearch = new Regex(flavorName, RegexOptions.IgnoreCase);
+        flavorQuery = flavorQuery.Where(f => flavorSearch.IsMatch(f.Name));
+      }
+      IEnumerable<Food> foodList = foodQuery
+        .ToList()
+        .OrderBy(f => f.Name);
+      IEnumerable<Flavor> flavorList = flavorQuery
+        .ToList()
+        .OrderBy(f => f.Name);
+      ViewBag.FoodList = foodList;
+      ViewBag.FlavorList = flavorList;
       return View();
     }
   }
