50d3e9559c3a30ee38c5b59ebae9daa74d884b20
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-14 16:59:01 -0700

    re-adding the appsetting.json file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <p align="center">
   <a href="" rel="noopener">
- <img width=200px height=200px src="https://i.imgur.com/6wj0hh6.jpg" alt="Project logo"></a>
+ <img width=640px height=423px src="./Treat/wwwroot/img/cinnamon_roll.jpg" alt="Project logo"></a>
 </p>
 
 <h3 align="center">Pierre's Treats</h3>
@@ -17,7 +17,6 @@
 - [Getting Started](#getting_started)
 - [Usage](#usage)
 - [Built Using](#built_using)
-- [TODO](../TODO.md)
 - [Authors](#authors)
 
 ## 🧐 About <a name = "about"></a>
@@ -33,30 +32,22 @@ These instructions will get you a copy of the project up and running on your loc
 ### Prerequisites
 
 To start, you'll need:
-1. MySQL Server
-2. .NET
+1. MySQL Server [Mac](https://dev.mysql.com/downloads/file/?id=484914)/[Windows](https://dev.mysql.com/downloads/file/?id=484919)
+2. [.NET framework](https://dotnet.microsoft.com/download/dotnet-core/2.2)
 3. A MySQL manager, such as MySQL Workbench (optional)
 4. A code editor, such as VS Code or Atom (optional)
-   
-
-```
-Give examples
-```
 
 ### Installing
 
 1. You'll need .NET Framework and MySQL server installed on your machine.
-2. Install the necessary packages by navigating into the Treats directory in your terminal (Powershell works for this) and running `dotnet restore`
-3. The {Password} in appsettings.json should be updated to your password for MySQL server.
-4. Then, install the database structure. There are three ways to do this:
+2. Download the repo by either clicking the download button, or running `git clone https://github.com/Sudolphus/Treat.Solution.git`
+3. Install the necessary packages by navigating into the Treats directory in your terminal (Powershell works for this) and running `dotnet restore`
+4. The {Password} in appsettings.json should be updated to your password for MySQL server.
+5. Then, install the database structure. There are three ways to do this:
    * Run the migration using Entity by running `dotnet ef database update` while in the Treats directory.
    * Import the micheal_hansen.sql file included within the Treat.Solution directory using your database manager.
-   * Run the following code in a terminal that has mysql running:
-```
-Give the example
-```
-  
-5. Once the database is installed, `dotnet build` should build the project in a bin/ folder. Alternately, `dotnet run` should make the project viewable in your browser on localhost:5000
+   * Type the commands as listed in the [micheal_hansen.sql](./micheal_hansen.sql) file into a terminal with my_sql running
+6. Once the database is installed, `dotnet build` should build the project in a bin/ folder. Alternately, `dotnet run` should make the project viewable in your browser on localhost:5000
 
 ## 🎈 Usage <a name="usage"></a>
 
@@ -64,10 +55,9 @@ Assuming the database is installed correctly in MySQL server, the `dotnet run` c
 
 ## ⛏️ Built Using <a name = "built_using"></a>
 
-- [MongoDB](https://www.mongodb.com/) - Database
-- [Express](https://expressjs.com/) - Server Framework
-- [VueJs](https://vuejs.org/) - Web Framework
-- [NodeJs](https://nodejs.org/en/) - Server Environment
+- [.NET Framwork](https://dotnet.microsoft.com/)
+- [MySql Server](https://dev.mysql.com/)
+- [Bootstrap](https://getbootstrap.com/)
 
 ## ✍️ Authors <a name = "authors"></a>
 
---
+++ micheal_hansen.sql
@@ -0,0 +1,110 @@
+DROP DATABASE IF EXISTS `micheal_hansen`;
+CREATE DATABASE  IF NOT EXISTS `micheal_hansen`;
+USE `micheal_hansen`;
+DROP TABLE IF EXISTS `__efmigrationshistory`;
+CREATE TABLE `__efmigrationshistory` (
+  `MigrationId` varchar(95) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetroles`;
+CREATE TABLE `aspnetroles` (
+  `Id` varchar(255) NOT NULL,
+  `Name` varchar(256) DEFAULT NULL,
+  `NormalizedName` varchar(256) DEFAULT NULL,
+  `ConcurrencyStamp` longtext,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `RoleNameIndex` (`NormalizedName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetusers`;
+CREATE TABLE `aspnetusers` (
+  `Id` varchar(255) NOT NULL,
+  `UserName` varchar(256) DEFAULT NULL,
+  `NormalizedUserName` varchar(256) DEFAULT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `NormalizedEmail` varchar(256) DEFAULT NULL,
+  `EmailConfirmed` bit(1) NOT NULL,
+  `PasswordHash` longtext,
+  `SecurityStamp` longtext,
+  `ConcurrencyStamp` longtext,
+  `PhoneNumber` longtext,
+  `PhoneNumberConfirmed` bit(1) NOT NULL,
+  `TwoFactorEnabled` bit(1) NOT NULL,
+  `LockoutEnd` datetime(6) DEFAULT NULL,
+  `LockoutEnabled` bit(1) NOT NULL,
+  `AccessFailedCount` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `UserNameIndex` (`NormalizedUserName`),
+  KEY `EmailIndex` (`NormalizedEmail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetuserclaims`;
+CREATE TABLE `aspnetuserclaims` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `UserId` varchar(255) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  PRIMARY KEY (`Id`),
+  KEY `IX_AspNetUserClaims_UserId` (`UserId`),
+  CONSTRAINT `FK_AspNetUserClaims_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetuserlogins`;
+CREATE TABLE `aspnetuserlogins` (
+  `LoginProvider` varchar(255) NOT NULL,
+  `ProviderKey` varchar(255) NOT NULL,
+  `ProviderDisplayName` longtext,
+  `UserId` varchar(255) NOT NULL,
+  PRIMARY KEY (`LoginProvider`,`ProviderKey`),
+  KEY `IX_AspNetUserLogins_UserId` (`UserId`),
+  CONSTRAINT `FK_AspNetUserLogins_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetuserroles`;
+CREATE TABLE `aspnetuserroles` (
+  `UserId` varchar(255) NOT NULL,
+  `RoleId` varchar(255) NOT NULL,
+  PRIMARY KEY (`UserId`,`RoleId`),
+  KEY `IX_AspNetUserRoles_RoleId` (`RoleId`),
+  CONSTRAINT `FK_AspNetUserRoles_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `aspnetroles` (`Id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_AspNetUserRoles_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetusertokens`;
+CREATE TABLE `aspnetusertokens` (
+  `UserId` varchar(255) NOT NULL,
+  `LoginProvider` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Value` longtext,
+  PRIMARY KEY (`UserId`,`LoginProvider`,`Name`),
+  CONSTRAINT `FK_AspNetUserTokens_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `aspnetroleclaims`;
+CREATE TABLE `aspnetroleclaims` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `RoleId` varchar(255) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  PRIMARY KEY (`Id`),
+  KEY `IX_AspNetRoleClaims_RoleId` (`RoleId`),
+  CONSTRAINT `FK_AspNetRoleClaims_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `aspnetroles` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `flavors`;
+CREATE TABLE `flavors` (
+  `FlavorId` int NOT NULL AUTO_INCREMENT,
+  `Name` longtext,
+  PRIMARY KEY (`FlavorId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `foods`;
+CREATE TABLE `foods` (
+  `FoodId` int NOT NULL AUTO_INCREMENT,
+  `Name` longtext,
+  PRIMARY KEY (`FoodId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `flavorfood`;
+CREATE TABLE `flavorfood` (
+  `FlavorFoodId` int NOT NULL AUTO_INCREMENT,
+  `FlavorId` int NOT NULL,
+  `FoodId` int NOT NULL,
+  PRIMARY KEY (`FlavorFoodId`),
+  KEY `IX_FlavorFood_FlavorId` (`FlavorId`),
+  KEY `IX_FlavorFood_FoodId` (`FoodId`),
+  CONSTRAINT `FK_FlavorFood_Flavors_FlavorId` FOREIGN KEY (`FlavorId`) REFERENCES `flavors` (`FlavorId`) ON DELETE CASCADE,
+  CONSTRAINT `FK_FlavorFood_Foods_FoodId` FOREIGN KEY (`FoodId`) REFERENCES `foods` (`FoodId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
