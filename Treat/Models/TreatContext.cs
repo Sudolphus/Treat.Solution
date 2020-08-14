e63d38979e3f1a3ef22c7a5bada65e245627e614
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-14 08:43:29 -0700

    add boilerplate files for program, startup, and dbcontext

---
+++ Treat/Models/TreatContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Treat.Models
+{
+  public class TreatContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public TreatContext(DbContextOptions options) : base(options) { }
+  }
+}
